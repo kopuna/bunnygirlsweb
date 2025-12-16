0ca700f1587e7826f5bc2bfa2036f0a113526df8
root <root@DESKTOP-L0OTNC2.localdomain>
2025-12-16 23:33:34 +0100

    broken commit of tryinf to add profiles but image loading is fucked up

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,8 +1,11 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
--- src/app/profiles/profile/profile.ts
+++ src/app/profiles/profile/profile.ts
@@ -1,11 +1,51 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Girl } from '../../utils/girl';
+import { GetGirlsService } from '../../utils/getGirlsService';
+import { ActivatedRoute } from '@angular/router';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './profile.html',
   styleUrl: './profile.css',
 })
-export class Profile {
+export class Profile implements OnInit {
+  girls$: Observable<Girl[]>;
+  girls: Girl[] = [];
+  girl: Girl | undefined;
+  name: string = '';
 
+  constructor(private getGirlsService: GetGirlsService, private cdr: ChangeDetectorRef, private route: ActivatedRoute) {
+    this.girls$ = this.getGirlsService.getGirls();
+  }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.name = params.get('name') || '';
+      
+      this.girls$.subscribe(values => {
+        this.girls = values;
+        this.girl = this.filterGirlsByName(this.name);
+      });
+    });
+    
+    this.cdr.detectChanges();
+  }
+  
+  filterGirlsByName(name: string): Girl | undefined {
+    return this.girls.find(girl =>
+      girl.name.toLowerCase().includes(name.toLowerCase())
+    );
+  }
+
+  getProfileImg(): string {
+    var lowerName: string = this.girl!.name.toLowerCase();
+    var link = this.girl?.galleryDestination + '/' + lowerName + '.png';
+    console.log(link);
+    return link;
+  }
 }
